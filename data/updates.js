5e10b65c026cdb93255e4ec9335f843ae45bd416
wsggVeylu <156438860+wsggVeylu@users.noreply.github.com>
2024-01-12 14:11:34 -0600

    Add files via upload

---
+++ data/updates.js
@@ -0,0 +1,32 @@
+[
+   {
+      "title":"🎮 Games Added",
+      "version":"v3.0.3",
+      "date":"11/12/23",
+      "content":"Added the following:<br />• Roblox <br />•Nowgg"
+   },
+   {
+      "title":"🔨Website Fix",
+      "version":"v3.0.2",
+      "date":"8/29/23",
+      "content":"Fixed the following:<br />• Flash Games (.swf) not working <br />•Fixed Advertisements not showing up<br />•Removed Roblox until further notice"
+   },
+   {
+      "title":"🎮 Games Added",
+      "version":"v3.0.1",
+      "date":"8/28/23",
+      "content":"We added some games and apps that are listed below. Enjoy and don't forget to do your homework. <br />• Microsoft Solitaire Collection <br />• OvO <br />• Snow Rider 3D"
+   },
+    {
+       "title":"💻 Complete Site Redesign",
+       "version":"v3.0",
+       "date":"8/26/23",
+       "content":"Welcome to the new ExtremeMath! We have decided to do a UI redesign and bring a new update to all of our users, enjoy all the new games and features!"
+    },
+    {
+       "title":"🎮 Games Added",
+       "version":"v2.9",
+       "date":"5/29/23",
+       "content": "We added some games and apps that are listed below. Enjoy and don't forget to do your homework. <br />• Monkey Mart <br />• Doomori <br />• Day 224 <br />• Penalty Shooters 2 <br />• The Legend of Zelda: Ocarina of Time <br />• Blumgi Slime <br />• Junon.io <br />• Pokemon Black <br />• Pokemon FireRed <br />• Pokemon LeafGreen <br />• Pokemon Crystal <br />• Pokemon Sapphire <br />• Pokemon Emerald <br />• Pokemon Platinum <br />• LoFi Room(Itch.io) <br />• Brawl Stars FPS <br />• VSCode(On Apps Page) <br />• Fnaf 2 <br />• Super Mario 64"
+    }
+ ]
